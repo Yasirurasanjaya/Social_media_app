212ed0239f4ea6ab5162285ba53e13f8a7495ead
yasirurasanjaya <yasirurasanjaya@gmail.com>
2023-03-10 14:03:09 +0530

    Login and out

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,27 @@
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import {auth} from "../config/firebase";
+import {useAuthState} from "react-firebase-hooks/auth"
+import {signOut} from "firebase/auth"
+import { async } from '@firebase/util';
+
 export const Navbar= ()=>{
+
+    const [user] = useAuthState(auth)
+
+    const signUserOut = async()=>{
+        await signOut(auth)
+    }
     return <div>
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
+        <div>
+            <p>{user?.displayName}</p>
+            {user && (
+                <>
+            <img src={user?.photoURL|| ""} width="100" height="100"/>
+            <button onClick={signUserOut}>Sign Out</button>
+            </>
+            )}
+        </div>
     </div>
-}
+}     
---
+++ src/config/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQpCI0Dgk6zyvirajfwBwWdl4Cd2pfylU",
+  authDomain: "social-media-c412f.firebaseapp.com",
+  projectId: "social-media-c412f",
+  storageBucket: "social-media-c412f.appspot.com",
+  messagingSenderId: "1073141414829",
+  appId: "1:1073141414829:web:a4d480a1a5965b4c3c3f24"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
